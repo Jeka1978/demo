75dcaa794440c17a9fcfb419e5e3e19aef5ee3d6
jeka1978 <kit2009>
2019-07-02 19:57:30 +0300

    pdf added

--- cashbox_lab/main.py
+++ cashbox_lab/main.py
@@ -4,6 +4,10 @@ from cashbox_lab.services import ProductService
 from linkedIn.SparkUtils import SparkHolder
 from pyspark.sql import functions as f
 
+BEER_NAME = 'beer name'
+
+CORRELATION = "correlation"
+
 PRODUCT_ID = 'product_id'
 
 CLIENT_ID = 'client_id'
@@ -13,15 +17,17 @@ CLIENT_ID = 'client_id'
 def getBeerName(id):
     return ProductService.dict[id].name
 
-
+@f.udf(returnType=IntegerType())
+def getBeerToNameCorrelation(beer_name,client_name):
+    return len(beer_name)+len(client_name)
 
 
 purchasesDf = SparkHolder.spark.read.json("input").alias('pp')
 clients = SparkHolder.spark.read.json("clients.json")
 join = purchasesDf.join(clients, purchasesDf[CLIENT_ID] == clients['id'])
-join = join.withColumn('beer name', getBeerName(PRODUCT_ID))\
+join = join.withColumn(BEER_NAME, getBeerName(PRODUCT_ID))\
     .withColumn("beer price", ProductService.getBeerPrice(PRODUCT_ID))\
-    .show()
+    .withColumn(CORRELATION,getBeerToNameCorrelation(BEER_NAME,'name')).show()
 
 #1. join dataframe from input file with cliens.json dataframe
 
--- top_words/TopWordService.py
+++ top_words/TopWordService.py
@@ -1,15 +1,19 @@
 from os import getcwd
-
+from pyspark.sql import functions as func
 from pyspark import SparkContext
 import re
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StringType, StructField
 
 sc = SparkContext("local", "taxi")
-
+spark = SparkSession(sparkContext=sc)
 
 
 def topX(path: str, x: int):
+    schema = StructType(
+        [StructField("word", StringType(), True)])
+
     with open(getcwd() + '\\garbage.txt') as f:
         stop_words = f.read().split(",")
     broadcast = sc.broadcast(stop_words)
@@ -17,16 +21,16 @@ def topX(path: str, x: int):
     wordsRdd = sc.textFile(path).map(lambda line: line.lower()) \
         .map(lambda line: re.sub(r"[^a-zA-Z0-9]+", ' ', line)) \
         .flatMap(lambda line: line.split(' ')) \
-        .filter(lambda word: len(word) > 1) \
-        .filter(lambda word:word not in broadcast.value)\
-        .map(lambda word: (word, 1)) \
-        .reduceByKey(lambda a, b: a + b) \
-        .map(lambda tuple: (tuple[1], tuple[0])) \
-        .sortByKey(ascending=False)\
+        .filter(lambda word: len(word) > 1)\
+        .map(lambda word:(word,1))
+    spark.createDataFrame(wordsRdd, schema=['word','amount'])\
+        .filter(~ func.col('word').isin(broadcast.value))\
+        .groupBy('word')\
+        .agg(func.sum(func.col('amount')).alias("total"))\
+        .orderBy('total',ascending=False).select(['word','total']).show()
 
 
-    for w in wordsRdd.collect():
-        print(w)
+
 
 
 topX("..\\songs\\pink floyd\\*", 3)
